484966175c152976f79a2e77d9b29f809de4b512
Kauã Haymon <kauahaymon@hotmail.com>
2025-02-13 19:53:23 -0400

    many to many: create third table

---
+++ db_cadastro/muitos-para-muitos.sql
@@ -0,0 +1,9 @@
+create table gafanhoto_assiste_curso (
+	id int not null auto_increment,
+    data date,
+    idgafanhoto int,
+    idcurso int,
+    primary key (id),
+    foreign key (idgafanhoto) references gafanhotos (id),
+    foreign key (idcuros) references cursos (idcurso)
+) default charset = utf8;
